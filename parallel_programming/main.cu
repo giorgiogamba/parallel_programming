3c09c9e18e4e2a3d7a86e77f63d0b96255a178d7
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-12-13 18:06:03 +0100

    add main test file

---
+++ parallel_programming/main.cu
@@ -0,0 +1,13 @@
+// Copyright Giorgio Gamba
+
+#include "vector_sum.cu"
+#include "matrix_multiplication.cu"
+#include "matrix_multiplication_tiled.cu"
+#include "sum_reduction.cu"
+
+int main()
+{
+	vector_sum();
+
+	return 0;
+}
